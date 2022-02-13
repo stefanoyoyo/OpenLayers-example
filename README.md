25913f220aa1af2e3941a788ff4179940b093f7a
stefanoyoyo <git.stefano@outlook.it>
2022-02-13 23:58:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular10-openlayers-demo-q1yhpl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular10-openlayers-demo-q1yhpl)
---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, VERSION, AfterViewInit } from "@angular/core";
+import "ol/ol.css";
+import Map from "ol/Map";
+import OSM from "ol/source/OSM";
+import TileLayer from "ol/layer/Tile";
+import View from "ol/View";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements AfterViewInit {
+  name = "Angular " + VERSION.major;
+  map: Map;
+
+  constructor() {
+    this.map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }
+
+  ngAfterViewInit() {
+    this.map.setTarget("map");
+  }
+}
